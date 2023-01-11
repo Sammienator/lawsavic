994e91180731daf668c1f7ae8d9d3fa4af6233a9
Sammienator <sammugo18@gmail.com>
2023-01-11 09:53:09 +0300

    changes again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
+import Nav from './components/Nav'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
+
+      <Nav/>
+      <About/>
+      
     </div>
   );
 }
